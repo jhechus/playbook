5d0bcdc85a8b37f9875d89b5379068106fdd279f
jhechus <jess.osm1412@gmail.com>
2022-04-16 15:46:09 -0500

    agregando ejercicio 2 de la semana 1 16/4/22

---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,22 @@
+// logger.js
+
+// Esta es una función que se guardara en este módulo como info
+exports.info = (message) => {
+    console.log(`info: ${message}`)
+  }
+  
+  // Esta es una función que se guardara en este módulo como verbose
+  exports.verbose = (message) => {
+    console.log(`verbose: ${message}`)
+  }
+
+  exports.test = (message) => {
+      console.log(`test: ${message}`)
+  }
+  
+  /*
+    const logger = require('./logger')
+    logger.info('This is an informational message')
+    logger.verbose('This is a verbose message')
+   * */
+  
